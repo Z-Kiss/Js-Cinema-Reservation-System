13868aae671c0109d02fd967f585f76de401253d
zotya <zoltankiss9210@gmail.com>
2023-07-05 20:20:30 +0200

    modify path

--- src/models/seatModel.js
+++ src/models/seatModel.js
@@ -1,5 +1,5 @@
 const {DataTypes} = require('sequelize')
-const sequelize = require('../config/database_connection.js')
+const sequelize = require('../config/databaseConnection.js')
 
 const SeatModel = sequelize.define('Seat', {
         id: {
