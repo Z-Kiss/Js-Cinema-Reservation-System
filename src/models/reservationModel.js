45908b90480eb08d6ea701bc8f5e4252086bdd38
zotya <zoltankiss9210@gmail.com>
2023-07-05 20:18:57 +0200

    add reservationModel

---
+++ src/models/reservationModel.js
@@ -0,0 +1,19 @@
+const {DataTypes} = require('sequelize')
+const sequelize = require('../config/databaseConnection.js')
+
+const ReservationModel = sequelize.define('Reservation', {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        seatId: {
+            type: DataTypes.CHAR(36),
+            allowNull: false
+        }
+    },
+    {
+        tableName: 'Reservations',
+        updatedAt: false,
+    });
+
+module.exports = ReservationModel
