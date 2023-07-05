9d82f41e44fbe709e71265d3a32e93052acdaac1
zotya <zoltankiss9210@gmail.com>
2023-07-05 18:55:51 +0200

    add database_connection.js and .env for creating connection to mysql database

---
+++ src/config/database_connection.js
@@ -0,0 +1,23 @@
+const {Sequelize} = require('sequelize');
+
+require('dotenv').config();
+
+const {MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_HOST} = process.env
+
+const sequelize = new Sequelize( {
+    username: MYSQL_USERNAME,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DATABASE,
+    host: MYSQL_HOST,
+    dialect: "mysql",
+    logging: false
+});
+
+sequelize.authenticate().then(() => {
+    console.log("Connection established successfully")
+})
+    .catch( error =>{
+        console.error('Unable to connect to the database', error)
+    })
+
+module.exports = sequelize;
