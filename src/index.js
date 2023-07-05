35e2426838ddaaa0a3362488327161e18b410850
zotya <zoltankiss9210@gmail.com>
2023-07-05 20:20:40 +0200

    modify path and add router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const compression = require('compression')
+const router = require('./controller/index')
 const http = require('http')
-const sequelize = require('./config/database_connection')
+const sequelize = require('./config/databaseConnection')
 const app = express();
+
 
 app.use(compression());
 app.use(bodyParser.json());
@@ -12,15 +14,13 @@ const server = http.createServer(app);
 
 sequelize.sync({force: false}).then(() => {
     console.log('All models are synchronized successfully')
-}).catch(error =>{
+}).catch(error => {
     console.error('Error occurred during model synchronization', error)
 })
+
 
 server.listen(8080, () => {
     console.log('Server running on http://localhost:8080')
 })
 
-
-
-
-
+app.use("/", router());
