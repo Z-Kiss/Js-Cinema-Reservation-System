598b6159a9c5c29a454af536644d0711c117a539
zotya <zoltankiss9210@gmail.com>
2023-07-06 14:11:09 +0200

    add payForReservation function

--- src/service/reservationService.js
+++ src/service/reservationService.js
@@ -1,8 +1,27 @@
 const ReservationRepository = require('../models/reservationModel')
 const {freeUpSeats} = require('../service/seatService')
+const {RESERVATION_TIME_LIMIT} = process.env
 const getAllReservation = async (req, res) =>{
     const reservations = await ReservationRepository.findAll()
     return res.status(200).json(reservations)
+}
+
+const payForReservation = async (req, res, next) =>{
+
+    const name = res.name;
+    const reservations = await ReservationRepository.findAll({where: {name: name}})
+    if(!reservations){
+        return res.status(400).send('You dont have reservations, try reserve again');
+    }
+    const seatsId = [];
+    for(const reservation of reservations){
+        let seatId = reservation.seatId;
+        seatsId.push(seatId);
+        reservation.paid = true;
+        await reservation.save();
+    }
+    res.seatsId = seatsId;
+    return next();
 }
 
 const makeReservation = async (req, res) =>{
@@ -15,8 +34,8 @@ const makeReservation = async (req, res) =>{
         scheduleTaskReservationChecking(name, seatsId);
         return res.status(200).send("Seats reserved, you have 2 minute to pay");
     }catch(error){
-        console.log(error)
-        return res.sendStatus(500)
+        console.log(error);
+        return res.sendStatus(500);
     }
 }
 
@@ -27,7 +46,6 @@ const scheduleTaskReservationChecking = (name, seatsId) =>{
 const cleanUpReservations = async (name, seatsId) =>{
     await deleteNotPaidReservations(name);
     await freeUpSeats(seatsId);
-    console.log('Reservation deleted');
 }
 
 const deleteNotPaidReservations = async (name) =>{
@@ -38,5 +56,6 @@ const deleteNotPaidReservations = async (name) =>{
 
 module.exports = {
     getAllReservation,
-    makeReservation
+    makeReservation,
+    payForReservation
 }
