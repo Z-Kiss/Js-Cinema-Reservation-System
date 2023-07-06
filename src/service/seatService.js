defbe5e3bf62b652474da5630feae81a1d50348f
zotya <zoltankiss9210@gmail.com>
2023-07-06 11:33:54 +0200

    add freeUpSeats and refactor seatService

--- src/service/seatService.js
+++ src/service/seatService.js
@@ -1,44 +1,46 @@
-const SeatService = require('../models/seatModel')
-const ReservationService = require('../models/reservationModel')
+const SeatRepository = require('../models/seatModel')
 
 const getAllSeat = async (req, res) => {
-    const seats = await SeatService.findAll()
+    const seats = await SeatRepository.findAll()
     return res.json(seats);
 }
 
-const reserveSeats = async (req, res) => {
-    const {seatsId, email} = req.body;
-
+const reserveSeats = async (req, res, next) => {
     try {
-        for (const seatId of seatsId) {
-            let seat = await SeatService.findByPk(seatId)
-            seat.status = 'pending';
+        const seats = res.seats;
+
+        for (const seat of seats) {
+            seat.status = "pending";
             await seat.save()
-            let reservation = await ReservationService.create({email: email, seatId: seatId})
-            console.log(reservation)
-            await reservation.save()
         }
-        return res.status(200).send('Seats Reserved');
+        return next();
     } catch (error) {
-        console.log(error)
-        return res.sendStatus(500)
+        console.log(error);
+        return res.sendStatus(500);
     }
 }
 const fillUpDatabaseWithSeats = async (req, res) => {
     const {amountOfRow, amountOfSeatInRow} = req.body;
     for (let i = 0; i < amountOfRow; i++) {
         for (let j = 0; j < amountOfSeatInRow; j++) {
-            await SeatService.create({row: i, number: j}).catch((error) => {
-                return res.status(500).send(error);
-            })
+            await SeatRepository.create({row: i, number: j});
         }
     }
     return res.status(200).send(amountOfSeatInRow * amountOfRow + " seat created");
 }
 
+const freeUpSeats = async (seatsId) =>{
+    for (const seatId of seatsId) {
+        let seat = await SeatRepository.findByPk(seatId);
+        seat.status = 'free';
+        await seat.save()
+    }
+}
 
 module.exports = {
     getAllSeat,
     reserveSeats,
-    fillUpDatabaseWithSeats
+    fillUpDatabaseWithSeats,
+    freeUpSeats,
+    reserveSeats
 }
