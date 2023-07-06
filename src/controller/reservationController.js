c985e14107d89b246bb16b76103fef8a727c9b2d
zotya <zoltankiss9210@gmail.com>
2023-07-06 11:34:50 +0200

    add endpoints

--- src/controller/reservationController.js
+++ src/controller/reservationController.js
@@ -1,7 +1,9 @@
-const {getAllReservation} = require('../service/reservationService')
-
+const {getAllReservation, makeReservation} = require('../service/reservationService')
+const {checkSeatStatus, checkEmail, checkNumberOfReservation} = require('../midlewares/index')
+const {reserveSeats} = require('../service/seatService')
 const reservationController = (router) =>{
     router.get('/reservation', getAllReservation)
+    router.post('/reservation', checkEmail, checkNumberOfReservation, checkSeatStatus, reserveSeats, makeReservation)
 }
 
 module.exports = reservationController
--- src/controller/seatController.js
+++ src/controller/seatController.js
@@ -1,9 +1,8 @@
-const {getAllSeat, fillUpDatabaseWithSeats, reserveSeats} = require('../service/seatService')
+const {getAllSeat, fillUpDatabaseWithSeats} = require('../service/seatService')
 
 const seatController = (router) =>{
     router.get('/seat', getAllSeat);
     router.post('/seat/fill', fillUpDatabaseWithSeats)
-    router.patch('/seat', reserveSeats);
 }
 
 module.exports = seatController
